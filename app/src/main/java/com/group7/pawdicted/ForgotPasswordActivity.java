f10182be533ae02d6c2b794a4d7123b7aadc0d3b
nghilinh2802 <linhldn22411c@st.uel.edu.vn>
2025-06-06 00:23:38 +0700

    Thêm login, fgpwd, change pwd, signup

---
+++ app/src/main/java/com/group7/pawdicted/ForgotPasswordActivity.java
@@ -0,0 +1,31 @@
+package com.group7.pawdicted;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_forgot_password);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void open_change_password(View view) {
+        Intent intent=new Intent(ForgotPasswordActivity.this,ChangeForgetPasswordActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/group7/pawdicted/SignupActivity.java
@@ -0,0 +1,31 @@
+package com.group7.pawdicted;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_signup);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void open_login(View view) {
+        Intent intent=new Intent(SignupActivity.this,LoginActivity.class);
+        startActivity(intent);
+    }
+}
