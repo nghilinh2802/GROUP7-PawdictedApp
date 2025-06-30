14f1285aca7cafe343972315cbd03f8c322e833b
chouchou76 <chaulnh22411c@st.uel.edu.vn>
2025-07-01 00:12:14 +0700

    noti done

--- app/src/main/java/com/group7/pawdicted/NotificationActivity.java
+++ app/src/main/java/com/group7/pawdicted/NotificationActivity.java
@@ -15,9 +15,9 @@ import androidx.fragment.app.Fragment;
 
 public class NotificationActivity extends AppCompatActivity {
 
-    private Button btnPromotions, btnOrders;
     private FrameLayout contentFrame;
     FooterManager footerManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,36 +28,12 @@ public class NotificationActivity extends AppCompatActivity {
             imgBack.setOnClickListener(v -> finish());
         }
 
-        btnPromotions = findViewById(R.id.btn_promotions);
-        btnOrders = findViewById(R.id.btn_orders);
         contentFrame = findViewById(R.id.notification_content);
-
-        setupTabs();
         footerManager = new FooterManager(this);
-    }
 
-    private void setupTabs() {
-        btnPromotions.setOnClickListener(v -> selectTab(true));
-        btnOrders.setOnClickListener(v -> selectTab(false));
-        selectTab(true); // Tab mặc định
+        // Chỉ hiển thị PromotionsFragment
+        loadFragment(new PromotionsFragment());
     }
-
-    private void selectTab(boolean isPromotions) {
-        if (isPromotions) {
-            btnPromotions.setBackgroundResource(R.drawable.tab_selected_left);
-            btnPromotions.setTextColor(Color.WHITE);
-            btnOrders.setBackgroundResource(R.drawable.tab_unselected_right);
-            btnOrders.setTextColor(Color.parseColor("#9C1B2C"));
-            loadFragment(new PromotionsFragment());
-        } else {
-            btnOrders.setBackgroundResource(R.drawable.tab_selected_right);
-            btnOrders.setTextColor(Color.WHITE);
-            btnPromotions.setBackgroundResource(R.drawable.tab_unselected_left);
-            btnPromotions.setTextColor(Color.parseColor("#9C1B2C"));
-            loadFragment(new OrdersFragment());
-        }
-    }
-
 
     private void loadFragment(Fragment fragment) {
         getSupportFragmentManager().beginTransaction()
@@ -65,3 +41,4 @@ public class NotificationActivity extends AppCompatActivity {
                 .commit();
     }
 }
+
